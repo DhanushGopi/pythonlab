460cacd6cba4593b7387f64193fbc71c2c11e60e
DhanushGopi <g.dhanushgopi1509@gmail.com>
2024-04-04 00:21:39 +0530

    Update

---
+++ 14/main.py
@@ -0,0 +1,9 @@
+
+import mymath
+
+list = [1,2,3,4,5]
+
+print("The Maximum ",mymath.maxfind(list))
+print("The Minimum ",mymath.minfind(list))
+print("The Sum ", mymath.calsum(list))
+print("The Avg ", mymath.calavg(list))
---
+++ 14/mymath.py
@@ -0,0 +1,15 @@
+
+#Write a Python Code to define a USER-DEFINED MODULE to FIND MAXIMUM, MINIMUM, SUM AND
+#AVERAGE OF GIVEN “n” NUMBERS.
+
+def maxfind(number):
+    return max(number)
+
+def minfind(number):
+    return min(number)
+
+def calsum(number):
+    return sum(number)
+
+def calavg(number):
+    return sum(number)/len(number)
